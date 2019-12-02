f4611607b772d3b35275231f35de493a52122756
Nastya Patutina <npatutina@gmail.com>
2019-12-03 01:49:31 +0300

    Delete c server

--- server/parser.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARSER_H
-#define PARSER_H 
-
-struct request * parse_request(char * message);
-int get_session_response(char* message);
-int check_domain(char * domain);
-char * get_email(char * argument);
-
-#endif
--- server/sender.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef SENDER_H
-#define SENDER_H
-
-void* sender_worker(void* data);
-
-#endif
