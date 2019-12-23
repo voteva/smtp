2001a7a9a4daf90fbf606d9e524bacb7fe68e145
voteva <tatianavoteva@gmail.com>
2019-12-23 22:31:52 +0300

    Add message fields

--- common/src/main/java/com/bmstu/nets/common/model/Message.java
+++ common/src/main/java/com/bmstu/nets/common/model/Message.java
@@ -2,7 +2,12 @@ package com.bmstu.nets.common.model;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Message {
+    private String sender;
+    private List<String> recipients;
+    private String subject;
     private byte[] data;
 }
